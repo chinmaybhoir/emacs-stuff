7022392a3d3280e590a17f0d9e955aabbc400fce
Chinmay Bhoir <chinmay.bhoir@atmecs.com>
2018-08-08 11:42:19 +0530

    Added init.el file

---
+++ init.el
@@ -0,0 +1,18 @@
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-file "~/.emacs.d/themes/spacemacs-theme/spacemacs-common.el")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/spacemacs-theme/")
+(load-theme 'spacemacs-dark t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "420459d6eeb45aadf5db5fbcc3d6990b65141c104911f7359454fc29fa9d87a0" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
